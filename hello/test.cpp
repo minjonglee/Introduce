a0dba38af169945e6395cf0c36419134066d20e9
minjonglee <prime5823@naver.com>
2019-12-04 18:48:15 +0900

    minjong

---
+++ hello/test.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define MAX 45
+int main()
+{
+	int i;
+	srand((unsigned)time(NULL));
+
+	for (i = 0;i < 6;i++)
+		printf("%d ", 1 + (rand() % MAX));
+	printf("\n");
+	return 0;
+}
